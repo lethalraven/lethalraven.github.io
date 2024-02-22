2bf532f02391030cf6012849db47977b99f29e60
Liam <liamjamesolivier@gmail.com>
2024-02-22 14:48:36 +0200

    Landing Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+
+const router = createBrowserRouter([
+  { path: "/", element: <LandingPage />, },
+]);
+
 const App: React.FC = () => {
   return (
     <div className="App">
-
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,12 @@
+const LandingPage: React.FC = () => {
+  return (
+    <div className="LandingPage">
+      <h1>Hi, I'm</h1>
+      <h1>Liam Olivier</h1>
+      <h1>Graduate Software Engineer</h1>
+      <h1>Welcome to my Portfolio</h1>
+    </div>
+  );
+};
+
+export default LandingPage;
